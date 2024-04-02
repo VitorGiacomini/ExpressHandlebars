dee6eb2672797d1a40e2d2c78ceff191f50d19d5
VitorGiacomini <vitor.giacomini@estudante.ifms.edu.br>
2024-04-02 16:33:02 -0300

    atualiza e adiciona campos de local e valor

--- app.js
+++ app.js
@@ -20,18 +20,24 @@ var eventos = [
 		nomeEvento: 'Jão SUPERTUNÊ',
 		dataEvento: '06-04-2024',
 		horaEvento: '22:00',
+		localEvento: 'São Paulo',
+		valorIngresso: 'R$245,00',
 	},
 	{
 		id: 2,
 		nomeEvento: 'IMPROVÁVEL - BARBIXAS',
 		dataEvento: '30-03-2024',
 		horaEvento: '19:00',
+		localEvento: 'Rio de Janeiro',
+		valorIngresso: 'R$245,00',
 	},
 	{
 		id: 3,
 		nomeEvento: 'SEPULTURA',
 		dataEvento: '08-08-2024',
 		horaEvento: '20:00',
+		localEvento: 'Rio de Janeiro',
+		valorIngresso: 'R$245,00',
 	},
 ];
 
@@ -42,7 +48,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/eventos/novo', (req, res) => {
-	res.render('forms', { cadastroOuAtt: true });
+	res.render('forms', { cadastroOuAtt: true, pagInicio: true });
 });
 
 app.get('/eventos/:id', (req, res) => {
@@ -50,7 +56,7 @@ app.get('/eventos/:id', (req, res) => {
 
 	const evento = eventos.find((evento) => evento.id === id);
 
-	res.render('evento', { evento, cadastroOuAtt: true });
+	res.render('evento', { evento, cadastroOuAtt: true, pagInicio: true });
 });
 app.get('/eventos/:id/atualizar', (req, res) => {
 	const id = parseInt(req.params.id);
@@ -60,6 +66,7 @@ app.get('/eventos/:id/atualizar', (req, res) => {
 		action: `/eventos/${id}/atualizar`,
 		isUpdate: true,
 		cadastroOuAtt: true,
+		pagInicio: true,
 	});
 });
 
@@ -68,6 +75,8 @@ app.post('/eventos/:id/atualizar', (req, res) => {
 	const nomeEvento = req.body.nomeEvento;
 	const dataEvento = req.body.dataEvento;
 	const horaEvento = req.body.horaEvento;
+	const localEvento = req.body.localEvento;
+	const valorIngresso = req.body.valorIngresso;
 
 	const index = eventos.findIndex((evento) => evento.id === id);
 	eventos[index] = {
@@ -75,6 +84,8 @@ app.post('/eventos/:id/atualizar', (req, res) => {
 		nomeEvento,
 		dataEvento,
 		horaEvento,
+		localEvento,
+		valorIngresso,
 	};
 
 	res.redirect('/');
@@ -84,12 +95,16 @@ app.post('/eventos/novo', (req, res) => {
 	const nomeEvento = req.body.nomeEvento;
 	const dataEvento = req.body.dataEvento;
 	const horaEvento = req.body.horaEvento;
+	const localEvento = req.body.localEvento;
+	const valorIngresso = req.body.valorIngresso;
 
 	eventos.push({
 		id: proximoId++,
 		nomeEvento: nomeEvento,
 		dataEvento: dataEvento,
 		horaEvento: horaEvento,
+		localEvento: localEvento,
+		valorIngresso: valorIngresso,
 	});
 
 	res.redirect('/');
